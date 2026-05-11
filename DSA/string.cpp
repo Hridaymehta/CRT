25a0f43898a721b96de78732927908fb6d61d2fb
Hriday Mehta <hridaymeht58@gmail.com>
2026-05-11 16:52:02 +0530

    Add string reversal and duplicate removal utilities
    Added `reverse_string`, `remove_duplicates_in_array`, and `duplicates` functions to string.cpp. Updated `solve()` to demonstrate these utilities and commented out the original string traversal.

--- DSA/string.cpp
+++ DSA/string.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 static void string_traversal(string& s) {
@@ -24,10 +26,62 @@ static void fun_function() {
 	cout << a << " " << b << endl;
 }
 
+static string reverse_string(string& s) {
+	string rev = s;
+	for (int i = 0; i < s.size(); i++) {
+		rev[i] = s[s.size() - i - 1];
+	}
+	return rev;
+}
+
+static void remove_duplicates_in_array() {
+	vector<int> v = {5,9,6,2,6,7,8,5,3,7,3,4};
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = i + 1; j < v.size(); j++) {
+			if (v[i] == v[j]) {
+				v.erase(v.begin() + j);
+				j--;
+			}
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = i + 1; j < v.size(); j++) {
+			if (v[i] > v[j]) {
+				swap(v[i], v[j]);
+			}
+		}
+	}
+	cout << endl;
+
+	cout << "After removing duplicates: ";
+	for (int i : v) {
+		cout << i << " ";
+	}
+
+	cout << endl;
+}
+
+static int duplicates(vector <int>& v) {
+	int d = 0;
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] != v[d]) {
+			v[++d] = v[i];
+		}
+	}
+	return d + 1;
+}
+
 static void solve() {
 	string s;
 	getline(cin, s);
-	string_traversal(s);
+	//string_traversal(s);
+	cout << reverse_string(s);
+
+	remove_duplicates_in_array();
+
+	vector<int> v = { 0,0,1,1,1,2,2,3,3,4 };
+	cout << "Number of unique elements: " << duplicates(v) << endl;
 
 	/*searching_in_string(s);
 	fun_function();*/
