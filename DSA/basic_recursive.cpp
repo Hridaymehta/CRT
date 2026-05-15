7d11f53c3e1371483a4976fd7cf43200e7ff83c7
Hriday Mehta <hridaymeht58@gmail.com>
2026-05-15 17:10:42 +0530

    Add recursive and memoized Fibonacci and factorial in C++
    Added basic_recursive.cpp with implementations for factorial and Fibonacci (both naive recursive and memoized). The main function demonstrates the memoized Fibonacci calculation for n=44.

---
+++ DSA/basic_recursive.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int fibonacci(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int fib_memo(int n) {
+	vector<int> memo(n + 1);
+	memo[0] = 0;
+	memo[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		memo[i] = memo[i - 1] + memo[i - 2];
+	}
+	return memo[n];
+}
+
+int main() {
+    int num = 44;
+    // cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    cout << "Fibonacci of " << num << " is " << fib_memo(num) << endl;
+    return 0;
+}
