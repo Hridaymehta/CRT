7746dcd186cdeb5cacf898ed258db69849794ad2
Hriday Mehta <hridaymeht58@gmail.com>
2026-05-15 17:23:47 +0530

    Initialize fib_memo vector with -1, test with n=10000
    Changed fib_memo to initialize its memoization vector with -1 to mark uncomputed entries. Updated main() to compute Fibonacci for n=10000 instead of 44 to test performance with larger inputs.

--- DSA/basic_recursive.cpp
+++ DSA/basic_recursive.cpp
@@ -17,7 +17,7 @@ int fibonacci(int n) {
 }
 
 int fib_memo(int n) {
-	vector<int> memo(n + 1);
+	vector<int> memo(n + 1, -1);
 	memo[0] = 0;
 	memo[1] = 1;
 	for (int i = 2; i <= n; i++) {
@@ -27,7 +27,7 @@ int fib_memo(int n) {
 }
 
 int main() {
-    int num = 44;
+    int num = 10000;
     // cout << "Factorial of " << num << " is " << factorial(num) << endl;
     cout << "Fibonacci of " << num << " is " << fib_memo(num) << endl;
     return 0;
