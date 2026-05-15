bf1a5a006f786266de1519ac6d01a4db55c3a47b
Hriday Mehta <hridaymeht58@gmail.com>
2026-05-15 15:46:23 +0530

    Implement singly linked list with basic operations
    Added Node and LinkedList structures in linked_list.cpp with methods for tail insertion, printing, searching, and length calculation. Declared but did not implement deleteNode. Included a main function to demonstrate usage. Updated binary a.exe.

---
+++ DSA/linked_list.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+#define forLL(curr) for (Node* curr = head; curr != nullptr; curr = curr->next)
+
+struct Node {
+public:
+    int data;
+    Node* next;
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+struct LinkedList {
+public:
+    struct Node* head;
+    LinkedList() : head(nullptr) {}
+    LinkedList(Node* h) : head(h) {}
+
+    void inserNodeTail(int val);
+    void deleteNode(int pos);
+    void print() const;
+    bool search(int target) const;
+    int length() const;
+};
+
+void LinkedList::inserNodeTail(int val) {
+    if (head == nullptr) {
+        head = new Node(val);
+        return;
+    }
+    Node* curr = head;
+    while (curr->next != nullptr) {
+        curr = curr->next;
+    }
+    curr->next = new Node(val);
+}
+
+void LinkedList::print() const {
+    forLL(curr) {
+        cout << curr->data << " ";
+    }
+    cout << endl;
+}
+
+int LinkedList::length() const {
+    int count = 0;
+    forLL(curr) {
+        count++;
+    }
+    return count;
+}
+
+bool LinkedList::search(int target) const {
+    forLL(curr) {
+        if (curr->data == target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void LinkedList::deleteNode(int pos) {
+
+}
+
+int main() {
+    LinkedList list;
+    for (int i = 1; i <= 5; i++) {
+        list.inserNodeTail(i);
+    }
+    list.print();
+    return 0;
+}
