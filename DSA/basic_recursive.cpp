023d254b17911de089f30d8ffc77ed2e89567689
Hriday Mehta <hridaymeht58@gmail.com>
2026-05-16 14:02:34 +0530

    Add space-optimized Fibonacci and make functions static
    Refactored factorial, fibonacci, and fib_memo as static functions to limit their scope. Added space_optimized_fib for efficient Fibonacci calculation. Updated main to use the new function and adjusted test value. Commented out previous fib_memo output. Minor code cleanup.

--- DSA/basic_recursive.cpp
+++ DSA/basic_recursive.cpp
@@ -2,21 +2,21 @@
 #include <vector>
 using namespace std;
 
-int factorial(int n) {
+static int factorial(int n) {
     if (n == 0) {
         return 1;
     }
     return n * factorial(n - 1);
 }
 
-int fibonacci(int n) {
+static int fibonacci(int n) {
 	if (n <= 1) {
 		return n;
 	}
 	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int fib_memo(int n) {
+static int fib_memo(int n) {
 	vector<int> memo(n + 1, -1);
 	memo[0] = 0;
 	memo[1] = 1;
@@ -26,9 +26,23 @@ int fib_memo(int n) {
 	return memo[n];
 }
 
+static int space_optimized_fib(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	int a = 0, b = 1;
+	for (int i = 2; i <= n; i++) {
+		int c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
+}
+
 int main() {
-    int num = 10000;
+    int num = 10;
     // cout << "Factorial of " << num << " is " << factorial(num) << endl;
-    cout << "Fibonacci of " << num << " is " << fib_memo(num) << endl;
+    // cout << "Fibonacci of " << num << " is " << fib_memo(num) << endl;
+    cout << "Space-optimized Fibonacci of " << num << " is " << space_optimized_fib(num) << endl;
     return 0;
 }
