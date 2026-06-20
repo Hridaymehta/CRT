34d5b3417c09f119e0612583f61c443d62e1f01c
Hriday Mehta <hridaymeht58@gmail.com>
2026-06-20 13:20:20 +0530

    Add searchRange function for finding target range in vector
    Added static searchRange function to binary_search.cpp to locate the first and last positions of a target value in a vector using binary search. Included the <vector> header for std::vector support.

--- CRT2/binary_search.cpp
+++ CRT2/binary_search.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+static vector<int> searchRange(vector<int>& nums, int target) {
+	int n = nums.size();
+	int l = 0;
+	int r = n - 1;
+	vector<int> result(2, -1);
+	while (l <= r) {
+		int mid = l + (r - l) / 2;
+		if (nums[mid] == target) {
+			result[0] = mid;
+			r = mid - 1;
+		}
+		else if (nums[mid] < target) {
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+	l = 0;
+	r = n - 1;
+	while (l <= r) {
+		int mid = l + (r - l) / 2;
+		if (nums[mid] == target) {
+			result[1] = mid;
+			l = mid + 1; 
+		}
+		else if (nums[mid] < target) {
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+	return result;
+}
 
 static void find_peak() {
 	const int n = 6;
